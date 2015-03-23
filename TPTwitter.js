ea7445f82fbf0dc7cacc6aa923c3ceb015ddb3c3
florent <florent.baudon@gmail.com>
2015-03-23 20:22:42 +0100

    Login fini - faire cookie

--- TPTwitter.js
+++ TPTwitter.js
@@ -13,8 +13,23 @@ app.get('/accueil', function(req,res){
 app.post('/tptwitter/login', urlencodedParser, function(req, res){
 	if(req.body.login !='' && req.body.passwd != ''){
 		//on récupére l'id du l'user à partir de son login
-		client.hget(req.body.login, function(err, reply) {
-			
+		client.hget('users', req.body.login, function(err, reply) {
+			var id = reply;
+			if(reply){
+				client.hget('user:'+id, 'password', function(err, reply){
+					var password = reply;
+					if(reply && password == req.body.passwd){
+						console.log('success login');
+						res.render('LogResult.ejs');
+					}else{
+						console.log('fail login');
+						//prevoir msg erreur
+						res.render('accueil.ejs', {msg: 'Echec de la connexion'});
+					}
+				});
+			}else{
+				res.render('accueil.ejs', {msg: 'Echec de la connexion'});
+			}
 		});
 	}
 	
@@ -28,7 +43,7 @@ app.post('/tptwitter/newuser', urlencodedParser, function(req, res){
 		//on verfifie que la confirmation du password est juste
 		if(req.body.newpasswd != req.body.newpasswd2){
 			//si elle ne l'est pas en renvoie à l'accueil (prévoir msg erreur)
-			res.render('accueil.ejs');
+			res.render('accueil.ejs', {msg2: 'Mots de passes différents'});
 		}else{
 			var id;
 			//on incrémente la variable userid
@@ -39,14 +54,14 @@ app.post('/tptwitter/newuser', urlencodedParser, function(req, res){
 					id = reply;
 					console.log("id : " + id);
 				}else{
-					res.render('noValue.ejs');
+					res.render('accueil.ejs', {msg2: 'Echec de l\'inscription, veuillez rééssayer plus tard'});
 				}
 				//ensuite on crée un hmset avec pour intitulé l'id et contenant le login et le mot de passe de l'utilisateur
 				client.hmset('user:'+id, 'username', req.body.newlogin, 'password', req.body.newpasswd);
 				//on crée un hset pour pouvoir retrouvé l'id de l'utilisateur à partir de son login
 				client.hset('users', req.body.newlogin, id);
 				//on redirige vers l'accueil (prévoir msg de confirmation)
-				res.redirect('/accueil');
+				res.render('accueil.ejs', {msg2: 'Vous êtes incrit, vous pouvez desormais vous connecter :)'});
 			});
 		}
 		
