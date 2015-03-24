34bc523df06d9054adc4674afb60f6c7abcd0c79
florent <florent.baudon@gmail.com>
2015-03-24 12:22:59 +0100

    login fini + interface boot strap jquery

--- TPTwitter.js
+++ TPTwitter.js
@@ -10,25 +10,24 @@ app.get('/accueil', function(req,res){
 	res.render('accueil.ejs', {msg: req.params.msg});
 });
 
+//SERVICEs REST
 app.post('/tptwitter/login', urlencodedParser, function(req, res){
 	if(req.body.login !='' && req.body.passwd != ''){
 		//on récupére l'id du l'user à partir de son login
+		console.log(req.body.login + '   ' + req.body.passwd);
 		client.hget('users', req.body.login, function(err, reply) {
 			var id = reply;
 			if(reply){
 				client.hget('user:'+id, 'password', function(err, reply){
 					var password = reply;
 					if(reply && password == req.body.passwd){
-						console.log('success login');
-						res.render('LogResult.ejs');
+						res.json({msg: 'success login'});
 					}else{
-						console.log('fail login');
-						//prevoir msg erreur
-						res.render('accueil.ejs', {msg: 'Echec de la connexion'});
+						res.json({msg: 'Echec de la connexion'});
 					}
 				});
 			}else{
-				res.render('accueil.ejs', {msg: 'Echec de la connexion'});
+				res.json({msg: 'Echec de la connexion'});
 			}
 		});
 	}
@@ -43,7 +42,7 @@ app.post('/tptwitter/newuser', urlencodedParser, function(req, res){
 		//on verfifie que la confirmation du password est juste
 		if(req.body.newpasswd != req.body.newpasswd2){
 			//si elle ne l'est pas en renvoie à l'accueil (prévoir msg erreur)
-			res.render('accueil.ejs', {msg2: 'Mots de passes différents'});
+			res.json({msg: 'Mots de passes différents'});
 		}else{
 			var id;
 			//on incrémente la variable userid
@@ -54,14 +53,14 @@ app.post('/tptwitter/newuser', urlencodedParser, function(req, res){
 					id = reply;
 					console.log("id : " + id);
 				}else{
-					res.render('accueil.ejs', {msg2: 'Echec de l\'inscription, veuillez rééssayer plus tard'});
+					res.json({msg: 'Echec de l\'inscription, veuillez rééssayer plus tard'});
 				}
 				//ensuite on crée un hmset avec pour intitulé l'id et contenant le login et le mot de passe de l'utilisateur
 				client.hmset('user:'+id, 'username', req.body.newlogin, 'password', req.body.newpasswd);
 				//on crée un hset pour pouvoir retrouvé l'id de l'utilisateur à partir de son login
 				client.hset('users', req.body.newlogin, id);
 				//on redirige vers l'accueil (prévoir msg de confirmation)
-				res.render('accueil.ejs', {msg2: 'Vous êtes incrit, vous pouvez desormais vous connecter :)'});
+				res.json({msg: 'Vous êtes incrit, vous pouvez desormais vous connecter :)'});
 			});
 		}
 		
